86155cfb9b7251dad0d45643bbe0c7bfcdff919b
tiger.long <tiger.long@hirede.com>
2018-01-02 18:12:59 +0800

    update ajax

--- common/common.js
+++ common/common.js
@@ -11,3 +11,61 @@ function distinctArray1(array){
 function distinctArray2(array){
     return [...new Set(array)];
 }
+//jquery ajax
+var $apis = {
+    get: function(url, successFn, errorFn){
+        $.ajax({
+            url: url,
+            type: 'GET',
+            contentType: 'application/json',
+            success: function(res){
+                successFn();
+            },
+            error: function(res){
+                errorFn(res);
+            }
+        })
+    },
+    post: function(url, data, successFn, errorFn){
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: data,
+            contentType: 'application/json',
+            success: function(res){
+                successFn && successFn(res);
+            },
+            error: function(res){
+                errorFn && errorFn(res);
+            }
+        })
+    },
+    put: function(url, data, successFn, errorFn){
+        $.ajax({
+            url: url,
+            type: 'PUT',
+            data: data,
+            contentType: 'application/json',
+            success: function(res){
+                successFn && successFn(res);
+            },
+            error: function(res){
+                errorFn && errorFn(res);
+            }
+        })
+    },
+    'delete': function(url, data, successFn, errorFn){
+        $.ajax({
+            url: url,
+            type: 'DELETE',
+            data: data,
+            contentType: 'application/json',
+            success: function(res){
+                successFn && successFn(res);
+            },
+            error: function(res){
+                errorFn && errorFn(res);
+            }
+        })
+    }
+};
