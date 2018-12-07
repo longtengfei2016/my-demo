7f603c11044f36d0eddda548cb061b78cd2693d3
龙腾飞(平安科技智慧城市智慧生活项目研发部) <longtengfei031@pingan.com.cn>
2018-12-07 17:47:54 +0800

    解决跨域问题

--- node/sentry/router/router.js
+++ node/sentry/router/router.js
@@ -1,6 +1,19 @@
 var router = require('express').Router()
 var request = require('../utils/request')
 
+router.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authentication,Origin, X-Requested-With, Content-Type, Accept,token");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    if(req.method == 'OPTIONS') {
+        //让options请求快速返回
+        res.sendStatus(200);
+    } else {
+        next(); 
+    }
+});
 router.use(function(req, res, next){
     console.log('router intercept')
     const m = new Date();
